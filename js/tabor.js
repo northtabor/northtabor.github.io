dd02dff965b827dbfdea2a4cfa10c704373190e9
Ahnaf Taha <ahnaftaha@Ahnafs-MacBook-Pro.local>
2017-08-09 23:24:38 -0400

    Copied NTabor website

---
+++ js/tabor.js
@@ -0,0 +1,42 @@
+
+$("#nav-tabor").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#landing").offset().top},
+        'slow');
+});
+
+$("#our").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#people").offset().top},
+        'slow');
+});
+
+$("#what").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+});
+
+$("#us").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
+
+$("#our1").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#people").offset().top},
+        'slow');
+});
+
+$("#what1").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+});
+
+$("#us1").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
